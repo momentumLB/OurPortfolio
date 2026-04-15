87cca144c599806c0959713c80e6c6d0b43d3e18
Jimmy <jimmy.hachem005@gmail.com>
2026-04-15 17:05:22 +0300

    google analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, useLocation } from "react-router-dom";
+import { useEffect } from "react";
 import { AppRoutes } from "./router";
 import { I18nextProvider } from "react-i18next";
 import i18n from "./i18n";
 import { StickyWhatsAppButton } from "./components/StickyWhatsAppButton";
 
+declare global {
+  interface Window { gtag: (...args: unknown[]) => void; }
+}
+
+function GATracker() {
+  const location = useLocation();
+  useEffect(() => {
+    window.gtag?.('config', 'G-RD7XMJWH4H', { page_path: location.pathname + location.search });
+  }, [location]);
+  return null;
+}
 
 function App() {
   return (
     <I18nextProvider i18n={i18n}>
       <BrowserRouter>
+        <GATracker />
         <AppRoutes />
         <StickyWhatsAppButton />
       </BrowserRouter>
